3ec2d4b07f891f66faf9039e92af22329fe06191
Harshitacharya <harshit.a.k.eee@gmail.com>
2020-09-26 19:44:40 +0530

    code refactor

--- src/main/java/com/example/springsocial/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.example.springsocial.service;
-
-import com.example.springsocial.model.Student;
-
-public interface StudentService {
-    Student getAllStudents();
-
-    Student getStudentById(int id);
-}
